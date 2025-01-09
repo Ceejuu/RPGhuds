80bb5c6a350816e4ad74f381e6af6ae29f21f2aa
Ceejuu <133282952+Ceejuu@users.noreply.github.com>
2025-01-09 17:13:41 +0100

    Update RPGHuds.java

--- src/main/java/dev/lone/rpghuds/core/RPGHuds.java
+++ src/main/java/dev/lone/rpghuds/core/RPGHuds.java
@@ -14,6 +14,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.Nullable;
+import dev.lone.rpghuds.core.data.SkyHud;
 
 import java.io.File;
 import java.io.IOException;
